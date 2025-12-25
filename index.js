cc8e2352974704762da1c8ae885531dbc21b2769
Kate Lee <80013939+Yunseolee0305@users.noreply.github.com>
2025-12-26 01:19:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <DarkOrLight /> 
+  //위의 내용을 각 chapter별 컴포넌트의 이름으로 계속 변경하면 됨
   </React.StrictMode>
 );
 
