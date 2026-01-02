910f39959817f426f58d0091bd9033c5a9f091cf
Kate Lee <80013939+Yunseolee0305@users.noreply.github.com>
2026-01-03 08:44:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DarkOrLight /> 
-  //위의 내용을 각 chapter별 컴포넌트의 이름으로 계속 변경하면 됨
+    <Blocks />
   </React.StrictMode>
 );
 
